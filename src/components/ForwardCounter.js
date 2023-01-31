5e923bc2932f2a53cfa2c4f93427db12007a4494
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-31 23:42:45 +0530

    configure the custom hook which can use different logic

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -5,7 +5,7 @@ import useCounter from "../hooks/use-counter";
 import Card from "./Card";
 
 const ForwardCounter = () => {
-    const counter = useCounter(0)
+    const counter = useCounter(true)
 
     return(
         <Card>{counter}</Card>
--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
 // in custom hooks function name must start with 'use' keyword
-const useCounter = () => {
+const useCounter = (forward = true) => {
     const [counter, setCounter] = useState(0)
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1)
+            if(forward) {
+                setCounter((prevCounter) => prevCounter + 1)
+            } else {
+                setCounter((prevCounter) => prevCounter - 1)
+            }
         }, 1000)
 
         return () => clearInterval(interval)
